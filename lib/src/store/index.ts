615b72c17705e618f6629513e92d609705b53f78
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-11-23 14:12:28 +0100

    Added persistant storage to Vuex with example

--- lib/src/store/index.ts
+++ lib/src/store/index.ts
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    comments: [{author: "Daniel Berg", body: "Some content"}, {author: "Daniel Berg", body: "This is the response to some content"}]
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  plugins: [createPersistedState()],
 });
