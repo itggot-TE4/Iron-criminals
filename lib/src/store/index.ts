59cd7695dc6c12fec1d73cd74873b9d53fd6d3c4
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-11-24 12:42:26 +0100

    Linting

--- lib/src/store/index.ts
+++ lib/src/store/index.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
-import journals from "./modules/journals"
+import journals from "./modules/journals";
 
 Vue.use(Vuex);
 
@@ -9,5 +9,5 @@ export default new Vuex.Store({
   modules: {
     journals: journals
   },
-  plugins: [createPersistedState()],
+  plugins: [createPersistedState()]
 });
--- lib/src/store/modules/journals.ts
+++ lib/src/store/modules/journals.ts
@@ -1,69 +1,80 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import store from "@/store/index"
+import store from "@/store/index";
 
 Vue.use(Vuex);
 
 interface Journal {
-  comment: Comment,
-  id: number,
-  student: number
-  date: Date
+  comment: Comment;
+  id: number;
+  student: number;
+  date: Date;
 }
 interface Comment {
-  id: number,
-  body: string,
-  author: number,
-  timestamp: Date
+  id: number;
+  body: string;
+  author: number;
+  timestamp: Date;
 }
 interface AddComment {
-  logID: number,
-  comment: string
+  logID: number;
+  comment: string;
 }
 interface RemoveComment {
-  logID: number,
-  commentID: number
+  logID: number;
+  commentID: number;
 }
 interface CreateComment {
-  logID: number,
-  body: string,
-  author: number,
+  logID: number;
+  body: string;
+  author: number;
 }
 
 export default {
   namespaced: true,
   state: {
-    journals: [{ id: 1, student: 1, date: new Date(), comments: [{author: 1, body: "Some content", id: 1, timestamp: new Date}] }]
+    journals: [
+      {
+        id: 1,
+        student: 1,
+        date: new Date(),
+        comments: [
+          { author: 1, body: "Some content", id: 1, timestamp: new Date() }
+        ]
+      }
+    ]
   },
   mutations: {
-    addComment (_state: any, args: AddComment) {
-      store.getters.journal(args.logID).comments.push(args.comment)
+    addComment(_state: any, args: AddComment) {
+      store.getters.journal(args.logID).comments.push(args.comment);
     },
-    removeComment (_state: any, args: RemoveComment) {
-      store.getters.journal(args.logID).comments = store.getters.journal(args.logID).comments.filter((x: Comment) => x.id !== args.commentID)
+    removeComment(_state: any, args: RemoveComment) {
+      store.getters.journal(args.logID).comments = store.getters
+        .journal(args.logID)
+        .comments.filter((x: Comment) => x.id !== args.commentID);
     }
   },
   actions: {
-    removeComment(_state: any, args: RemoveComment ) {
-      store.dispatch('removeComment', args)
+    removeComment(_state: any, args: RemoveComment) {
+      store.dispatch("removeComment", args);
     },
-    createComment(state: any, args: CreateComment ) {
+    createComment(state: any, args: CreateComment) {
       const comments = store.getters.journal(args.logID).comments as Comment[];
       const comment: Comment = {
-        id: (comments[comments.length -1].id + 1),
+        id: comments[comments.length - 1].id + 1,
         body: args.body,
         author: args.author,
-        timestamp: new Date
-      }
-      state.dispatch('addComment', comment );
+        timestamp: new Date()
+      };
+      state.dispatch("addComment", comment);
     }
   },
   getters: {
     journals: (state: any) => {
-      return state.journals
+      return state.journals;
     },
     journal: (state: any) => (logID: number) => {
       return state.journals.find((x: Journal) => x.id === logID);
     }
-  },
+  }
 };
