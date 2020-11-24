3aa74929a23169212e911fedeb1757d9e81bffe0
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-11-24 12:39:54 +0100

    Added vuex Journal module

--- lib/src/store/index.ts
+++ lib/src/store/index.ts
@@ -1,18 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
+import journals from "./modules/journals"
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    comments: [
-      { author: "Daniel Berg", body: "Some content" },
-      { author: "Daniel Berg", body: "This is the response to some content" }
-    ]
+  modules: {
+    journals: journals
   },
-  mutations: {},
-  actions: {},
-  modules: {},
-  plugins: [createPersistedState()]
+  plugins: [createPersistedState()],
 });
