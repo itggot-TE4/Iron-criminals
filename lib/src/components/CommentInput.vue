1cf1428797d4a7e848af33a544941da2a6071c1c
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-11-27 10:04:41 +0100

    Added the abiity for the comment component to send comments

--- lib/src/components/CommentInput.vue
+++ lib/src/components/CommentInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-col cols="12">
-      <v-textarea filled color="teal">
+      <v-textarea filled color="teal" v-model="comment">
         <template v-slot:label>
           <div>
             Comment
@@ -12,10 +12,8 @@
     <div class="d-flex justify-end">
       <v-btn
         class="ma-2"
-        :loading="loading2"
-        :disabled="loading2"
         color="success"
-        @click="loader = 'loading2'"
+        @click="onClick"
       >
         Send Comment
         <template v-slot:loader>
@@ -28,8 +26,24 @@
 
 <script lang="ts">
 import Vue from "vue";
+import store from "../store/index";
 
 export default Vue.extend({
-  props: ["logID"]
+  props: ["journal", "week"],
+  data() {
+    return {
+      comment: ""
+    };
+  },
+  methods: {
+    onClick: function() {
+      store.dispatch("journals/createComment", {
+        logID: this.journal.id,
+        yearWeek: this.week,
+        body: this.comment,
+        author: store.getters["auth/auth"].id
+      });
+    }
+  }
 });
 </script>
